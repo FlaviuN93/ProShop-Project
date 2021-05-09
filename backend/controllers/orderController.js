510967c5c6b2e7f073fa0247e494098f19545986
Flaviu Nemes <nemes.flaviu@gmail.com>
2021-05-09 22:38:45 +0300

    My Orders Functionality

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -76,4 +76,12 @@ const updateOrderToPaid = asyncAsyncHandler(async (req, res) => {
 	}
 });
 
-export { addOrderItems, getOrderById, updateOrderToPaid };
+// @desc Get logged in user orders
+// @route Get /api/orders/myorders
+// @access Private
+
+const getMyOrders = asyncAsyncHandler(async (req, res) => {
+	const orders = await Order.find({ user: req.user._id });
+	res.json(orders);
+});
+export { addOrderItems, getOrderById, updateOrderToPaid, getMyOrders };
